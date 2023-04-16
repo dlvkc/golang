e4c419c6827c397a8b00006334ab3e4fb35e3996
jerryoop <129260746+jerryoop@users.noreply.github.com>
2023-04-16 20:50:21 +1200

    Update main.go

--- main.go
+++ main.go
@@ -6,26 +6,17 @@ import (
     "net/http"
     "os"
     "os/exec"
+    "strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path == "/list" {
-        out, err := exec.Command("cat", "list").Output()
-        if err != nil {
-            fmt.Fprintf(w, "<html><body>Error: %s</body></html>", err.Error())
-        } else {
-            fmt.Fprintf(w, "<html><body>%s</body></html>", string(out))
-        }
-    } else {
-        fmt.Fprintln(w, "Hello, World!")
-    }
-}
-
 func main() {
+    // 设置根路径的处理程序
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // 将 "Hello, world" 写入响应
         w.Write([]byte("Hello, world"))
 
-        cmd := exec.Command("/bin/sh", "./entrypoint.sh")
+        // 执行 entrypoint.sh 脚本并输出结果
+        cmd := exec.Command("/bin/bash", "./entrypoint.sh")
         output, err := cmd.Output()
 
         if err != nil {
@@ -35,8 +26,22 @@ func main() {
         fmt.Println(string(output))
     })
 
-    http.HandleFunc("/list", handler)
+    // 设置 /list 的处理程序
+    http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+        cmd := "cat list"
+        output, err := exec.Command("bash", "-c", cmd).CombinedOutput()
+        if err != nil {
+            errorMsg := fmt.Sprintf("<h2>Error executing command:</h2><p>%s</p>", err)
+            w.Write([]byte(errorMsg))
+            return
+        }
+        outputString := string(output)
+        outputString = strings.Replace(outputString, "n", "<br>", -1) // 修改此处
+        htmlOutput := fmt.Sprintf("<h2>%s:</h2><p>%s</p>", cmd, outputString)
+        w.Write([]byte(htmlOutput))
+    })
 
+    // 启动 HTTP 服务器并监听端口
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
