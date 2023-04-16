3a133275043395f87eb671e4fd09c4381dcaaacd
jerryoop <129260746+jerryoop@users.noreply.github.com>
2023-04-16 22:16:29 +1200

    Update main.go

--- main.go
+++ main.go
@@ -10,45 +10,37 @@ import (
 )
 
 func main() {
+    // 设置根路径的处理程序
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        defer func() {
-            if r := recover(); r != nil {
-                log.Printf("Recovering from panic: %v", r)
-                http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-            }
-        }()
+        // 将 "Hello, world" 写入响应
+        w.Write([]byte("Hello, world"))
 
         // 执行 entrypoint.sh 脚本并输出结果
         cmd := exec.Command("/bin/bash", "./entrypoint.sh")
         output, err := cmd.Output()
+
         if err != nil {
             log.Fatal(err)
         }
 
-        // Write response at the end
-        defer func() {
-            if w.err == nil {
-                w.Write([]byte("Hello, world"))
-            }
-        }()
-
-        // Prepend output to response
-        w.Write(output)
+        fmt.Println(string(output))
     })
 
+    // 设置 /list 的处理程序
     http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
         cmd := "cat list"
         output, err := exec.Command("bash", "-c", cmd).CombinedOutput()
         if err != nil {
             errorMsg := fmt.Sprintf("<h2>Error executing command:</h2><p>%s</p>", err)
-            http.Error(w, errorMsg, http.StatusInternalServerError)
+            w.Write([]byte(errorMsg))
             return
         }
-        outputString := strings.ReplaceAll(string(output), "n", "n")
+        outputString := strings.Replace(string(output))
         htmlOutput := fmt.Sprintf("<h2>%s:</h2><p>%s</p>", cmd, outputString)
         w.Write([]byte(htmlOutput))
     })
 
+    // 启动 HTTP 服务器并监听端口
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
